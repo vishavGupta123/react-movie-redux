d5556d6763e58dfcbe093c4a34050aef33a2adc6
Vishav <vishavgupta@bubusiness.in>
2021-03-19 15:31:35 +0530

    Added the functionality to unfavourite movie and changing tabs

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
-import { ADD_MOVIES, ADD_FAVOURITE, REMOVE_FAVOURITE } from "../actions";
+import { ADD_MOVIES, ADD_FAVOURITE, REMOVE_FAVOURITE, SET_SHOW_FAVOURITES } from "../actions";
 
 const initialMoviesState = {
     list:[],
-    favourites: []
+    favourites: [],
+    showFavourites:false
 }
 
 export default function movies (state=initialMoviesState,action){
@@ -33,6 +34,11 @@ export default function movies (state=initialMoviesState,action){
             return {
                 ...state,
                 favourites:filteredArray
+            }
+        case SET_SHOW_FAVOURITES:
+            return {
+                ...state,
+                showFavourites:action.val
             }
         default:
             return state
