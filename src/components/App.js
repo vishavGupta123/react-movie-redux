ba075aa596c084beb38d663a87a94932096874ae
Vishav <vishavgupta@bubusiness.in>
2021-03-19 14:48:01 +0530

    changing the store state from array to object

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 import { data } from './data';
+import {addMovies} from '../actions/index';
 
 
 class App extends React.Component {
@@ -13,16 +14,14 @@ class App extends React.Component {
     })
     //make api call
     //dispatch action
-    store.dispatch({
-      type:'ADD_MOVIES',
-      movies: data
-    });
+    store.dispatch(addMovies(data));
 
     console.log('State',this.props.store.getState());
   }
   render(){
-    console.log('RENDER');
-    const movies = this.props.store.getState();
+    const {list} = this.props.store.getState() // {list: [],favourites: []};
+    console.log('RENDER',this.props.store.getState());
+
   return (
     <div className="App">
       <Navbar/>
@@ -33,7 +32,7 @@ class App extends React.Component {
         </div>
 
         <div className="list">
-          {movies.map((movie,index)=>(
+          {list.map((movie,index)=>(
             <MovieCard movie={movie} key={`movies-${index}`} />
           ))}
         </div>
